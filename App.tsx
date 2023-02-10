96624f3b60b69ae20f3fa8d1eb6a82d909cad7d9
yogendramaarisetty <43344815+yogendramaarisetty@users.noreply.github.com>
2023-02-10 16:23:28 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,103 @@
+/** @jsx jsx */
+
+import { jsx } from '@emotion/react';
+
+import { token } from '@atlaskit/tokens';
+
+import {
+  Banner,
+  Content,
+  LeftSidebarWithoutResize,
+  Main,
+  PageLayout,
+  RightPanel,
+  TopNavigation,
+} from '@atlaskit/page-layout';
+import {
+  SlotLabel,
+  SlotWrapper,
+} from './design-system/page-layout/examples/common';
+
+const BasicGrid = () => {
+  return (
+    <PageLayout>
+      {
+        <Banner
+          testId="banner"
+          id="banner"
+          skipLinkTitle="Banner"
+          height={60}
+          isFixed={false}
+        >
+          <SlotWrapper
+            borderColor={token('color.border.accent.yellow', 'gold')}
+          >
+            <SlotLabel>Banner</SlotLabel>
+          </SlotWrapper>
+        </Banner>
+      }
+      {
+        <TopNavigation
+          testId="topNavigation"
+          id="product-navigation"
+          skipLinkTitle="Product Navigation"
+          height={60}
+          isFixed={false}
+        >
+          <SlotWrapper borderColor={token('color.border.accent.blue', 'blue')}>
+            <SlotLabel>Product Navigation</SlotLabel>
+          </SlotWrapper>
+        </TopNavigation>
+      }
+      <Content testId="content">
+        {
+          <LeftSidebarWithoutResize
+            testId="leftSidebar"
+            id="space-navigation"
+            skipLinkTitle="Space Navigation"
+            isFixed={false}
+            width={125}
+          >
+            <SlotWrapper
+              borderColor={token('color.border.accent.green', 'darkgreen')}
+              hasExtraPadding
+            >
+              <SlotLabel isSmall>Space Navigation</SlotLabel>
+            </SlotWrapper>
+          </LeftSidebarWithoutResize>
+        }
+        {
+          <Main testId="main" id="main" skipLinkTitle="Main Content">
+            <SlotWrapper
+              borderColor={token('color.border', 'black')}
+              minHeight={400}
+            >
+              <SlotLabel isSmall>Main Content</SlotLabel>
+              <p>
+                Visit the first focusable element on the page to see the skip
+                links menu
+              </p>
+            </SlotWrapper>
+          </Main>
+        }
+      </Content>
+      {
+        <RightPanel
+          testId="rightPanel"
+          id="help-panel"
+          skipLinkTitle="Help Panel"
+          isFixed={false}
+          width={125}
+        >
+          <SlotWrapper
+            borderColor={token('color.border.accent.orange', 'orange')}
+          >
+            <SlotLabel>Help Panel</SlotLabel>
+          </SlotWrapper>
+        </RightPanel>
+      }
+    </PageLayout>
+  );
+};
+
+export default BasicGrid;
